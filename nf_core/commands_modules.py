0a6332610f6336bc03c9d1064355d3cabe6c6a98
mashehu <mashehu3@gmail.com>
2026-01-22 16:13:34 +0100

    clean up stale conda-lock files

--- nf_core/commands_modules.py
+++ nf_core/commands_modules.py
@@ -390,7 +390,7 @@ def modules_containers_create(ctx, module, directory, await_build: bool):
                 "[bold blue]{task.description}",
                 rich.progress.BarColumn(bar_width=None),
                 "[magenta]{task.completed} of {task.total}[reset]",
-                transient=False,
+                transient=True,
                 console=console,
                 disable=ctx.obj["hide_progress"],
             )
@@ -433,7 +433,7 @@ def modules_containers_create(ctx, module, directory, await_build: bool):
                 "[bold blue]{task.description}",
                 rich.progress.BarColumn(bar_width=None),
                 "[magenta]{task.completed} of {task.total}[reset]",
-                transient=False,
+                transient=True,
                 console=console,
                 disable=ctx.obj["hide_progress"],
             )
