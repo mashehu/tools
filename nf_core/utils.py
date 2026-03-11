ef8ab0f98b8a414a8d4f1c8b4d9a59148a619697
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-03-11 14:49:42 +0100

    Don't ever return None from nextflow_inspect

--- nf_core/utils.py
+++ nf_core/utils.py
@@ -366,7 +366,7 @@ def check_nextflow_version(minimal_nf_version: tuple[int, int, int, bool], silen
     return nf_version >= minimal_nf_version
 
 
-def nextflow_inspect(main_nf: Path, format: str = "json", profile: str = "docker") -> dict | None:
+def nextflow_inspect(main_nf: Path, format: str = "json", profile: str = "docker") -> dict:
     if not check_nextflow_version(NF_INSPECT_MIN_NF_VERSION):
         raise ValueError(
             f"Nextflow inspect cannot be run with this version of nextflow. nextflow >={NF_INSPECT_MIN_NF_VERSION} required"
@@ -391,7 +391,7 @@ def nextflow_inspect(main_nf: Path, format: str = "json", profile: str = "docker
 
         if cmd_out is None:
             log.debug(f"Failed to run `{executable} {cmd_params}`")
-            return None
+            return dict()
 
         out, _ = cmd_out
         out_str = str(out, encoding="utf-8")
